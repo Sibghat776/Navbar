9b746bdcf0ce0b0456b3cf4703535e02aa9ba8b9
Sibghat776 <ullahsibghat786@gmail.com>
2024-11-11 11:02:24 +0500

    first commit

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ function sideBar() {
     sideBar.style.display = 'flex'
 }
 function close() {
-    
-}
+    let sideBar = document.querySelector(".sideBar")
+    sideBar.style.display = 'none'
+}   
