296aa0b42a07f87da23547f6cbf5fde5c71fffae
Sibghat776 <ullahsibghat786@gmail.com>
2024-11-14 16:38:00 +0500

    Project Complete

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-function sideBar() {
-    console.log("Chala")
-    let sideBar = document.querySelector(".sideBar")
-    sideBar.style.display = 'flex'
+let menu = document.querySelector('#menu')
+let lists = document.querySelectorAll('li')
+function navbar() {
+    if (menu.style.maxWidth == '0px') {
+        menu.style.maxWidth = '60vw';
+        
+    } else {
+        menu.style.maxWidth = '0px'
+        document.querySelector('.lis1').classList.add('noBorderBottom')
+        document.querySelector('.lis2').classList.add('noBorderBottom');
+        document.querySelector('.lis3').classList.add('noBorderBottom');
+        document.querySelector('.lis4').classList.add('noBorderBottom');
+        
+    }
 }
-function close() {
-    let sideBar = document.querySelector(".sideBar")
-    sideBar.style.display = 'none'
-}   
